288ff6b2a3340c9432b1986145781a223e25bcd0
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-11-09 09:21:29 -0500

    Commit: "Using the 'checkNumber' method to pass through integers as arguments to check for certain conditions"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
     public static void main(String[] args) {  // use "psvm" + tab to create a 'main' method
         checkNumber(5);
+        checkNumber(-10);
     }
 
     public static void checkNumber(int number) {
         if(number > 0) {
             System.out.println("positive");
+        } else if(number < 0) {
+            System.out.println("negative");
         }
     }
 }
