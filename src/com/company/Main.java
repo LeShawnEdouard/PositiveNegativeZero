e0f48ae38cb4d6f9be64a7d407e307b75bf2a092
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-11-09 09:16:56 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {  // use "psvm" + tab to create a 'main' method
+        checkNumber(5);
+    }
+
+    public static void checkNumber(int number) {
+        if(number > 0) {
+            System.out.println("positive");
+        }
+    }
+}
